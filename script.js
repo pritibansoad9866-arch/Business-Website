dbaf7e6b8dd255847e0df65d57f31ed24e333f07
pritibansoad9866-arch <pritibansoad9866@gmail.com>
2026-04-09 22:51:02 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mobile Navigation Toggle
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+// Close
